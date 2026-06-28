d2d44c360eadbc235447d6baff6b8f4d91678f70
mike <118577094+mikechen04@users.noreply.github.com>
2026-06-27 22:18:04 -0700

    Add: sort-an-array - submission-1

---
+++ Data Structures & Algorithms/sort-an-array/submission-1.py
@@ -0,0 +1,21 @@
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def quickSort(nums):
+            if len(nums) <= 1:
+                return nums
+            
+            pivot = nums[len(nums) // 2]
+            left, mid, right = [], [], []
+
+            for num in nums:
+                if num < pivot:
+                    left.append(num)
+                elif num > pivot:
+                    right.append(num)
+                else:
+                    mid.append(num)
+            
+            return quickSort(left) + mid + quickSort(right)
+        
+        sorted = quickSort(nums)
+        return sorted
