9e995ba16f3ece3499a4a3d82e724c3742e7e17a
mike <118577094+mikechen04@users.noreply.github.com>
2026-08-27 23:02:35 +0900

    Add: invert-a-binary-tree - submission-1

---
+++ Data Structures & Algorithms/invert-a-binary-tree/submission-1.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def helper(node): # issue is that its only doing it to the first layer
+            if not node:
+                return []
+            else:
+                node.left, node.right = node.right, node.left
+                helper(node.left)
+                helper(node.right)
+                # node.left, node.right = node.left, node.right
+        
+        helper(root)
+        return root
